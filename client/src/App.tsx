b8953315b1d370771da20c8510adbe2ab92292a2
Michael Duren <michaeld@michaelduren.com>
2023-05-20 14:34:09 -0500

    added toast style and container to App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,28 @@
 import { Outlet } from 'react-router-dom';
 import NavBar from './routes/navigation/NavBar';
 import Layout from './components/layout/Layout';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   return (
-    <Layout>
-      <NavBar />
-      <Outlet />
-    </Layout>
+    <>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+      <Layout>
+        <NavBar />
+        <Outlet />
+      </Layout>
+    </>
   );
 }
 
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import './index.css';
+import 'react-toastify/dist/ReactToastify.min.css';
 import { StoreContext, store } from './stores/store.ts';
 import { router } from './routes/router/Routes.tsx';
 
