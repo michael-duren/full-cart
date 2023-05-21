675a31b26c451933b6612a8d65c06aba4515c6d3
Michael Duren <michaeld@michaelduren.com>
2023-05-21 15:33:00 -0500

    added user account api endpoint

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -1,10 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.DTOs
 {
     public class RegisterDto
     {
+        [Required]
         public string Email { get; set; }
+
+        [Required]
+        [RegularExpression(
+            @"[a-zA-Z''-'\d!@#$%^&]{8,40}",
+            ErrorMessage = "Password must container an upper and lower case letter, a number, and a special character. Password also must be 8 characters or more."
+        )]
         public string Password { get; set; }
+
+        [Required]
+        [EmailAddress]
         public string DisplayName { get; set; }
+
+        [Required]
         public string UserName { get; set; }
     }
 }
