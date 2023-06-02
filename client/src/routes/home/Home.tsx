e54572021a42bf75d2e6e4cd1a7337c6bec06ef0
Michael Duren <michaeld@michaelduren.com>
2023-06-02 06:59:58 -0500

    Created welcome screen for login

--- client/src/routes/home/Home.tsx
+++ client/src/routes/home/Home.tsx
@@ -4,10 +4,10 @@ import SecondaryButton from '../../components/buttons/SecondaryButton';
 
 export default function HomePage() {
   return (
-    <main className="flex  items-center justify-center h-[60vh]">
+    <main className="flex items-center justify-center h-[60vh]">
       <div className="flex flex-col items-center justify-center">
-        <h2 className="text-5xl main-gradient-text">fullCart</h2>
-        <div className="flex gap-4 mt-16">
+        <h2 className="logo-animation text-5xl main-gradient-text">fullCart</h2>
+        <div className="flex sign-in-animation gap-4 mt-16">
           <Link to={'login'}>
             <PrimaryButton type="button" content="Login" />
           </Link>
